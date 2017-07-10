69398f44effc82f96ac0602e4316293b954370b7
danielvivacqua <drvivacqua@hotmail.com>
2017-07-10 14:30:14 -0400

    Added Cat class

--- IntroToOOPSummer17/Program.cs
+++ IntroToOOPSummer17/Program.cs
@@ -10,6 +10,21 @@ namespace IntroToOOPSummer17
     {
         static void Main(string[] args)
         {
+            //When we initialize a new object, it is called INSTANTIATING (creating a new instance)
+            //An object needs to be instantiated before it can be used
+            //All objects of a specific type are created from the same template - CLASS
+            //All objects from a class will have the same number and type of states, BUT ...
+            //each instance can have it's own unique values.
+
+            //the format we use to instantiate an object looks like this:
+            //ClassName objectName = new ClassName();
+            Cat mittens = new Cat();
+            
+            //To create a new object, we use a special method called a CONSTRUCTOR
+
+            //Thinking about objects in real life, create (either hand-write or type)
+            //three examples of objects. Your objects should have five states (characteristics)
+            //and five behaviors.
         }
     }
 }
